1fbe3cc748bfd0af665895a95d317d77aa6011e2
StabijJack <jj.schneider@live.nl>
2020-03-28 12:11:02 +0100

    inital commit

---
+++ src/nl/famschneider/PHPSerializedDataReader/PHPArrayReader.java
@@ -0,0 +1,258 @@
+package nl.famschneider.PHPSerializedDataReader;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PHPArrayReader {
+    private Integer pointer;
+    private StringBuilder phpArraySerial;
+    private Map<String, Object> fieldMap;
+
+    public PHPArrayReader(String phpArraySerial) {
+        this.pointer = 0;
+        this.phpArraySerial = new StringBuilder(phpArraySerial);
+        this.fieldMap = new HashMap<>();
+        fillArrayFieldStructure();
+    }
+
+    public Map<String, Object> getFieldMap() {
+        return fieldMap;
+    }
+
+    private Map<String, Object> getArray(String[] options) throws PHPArrayReaderExeption {
+        Map<String, Object> fieldMap = this.fieldMap;
+        for (int i = 0; i < options.length - 1; i++) {
+            fieldMap = getOptionArray(options[i], fieldMap);
+        }
+        return fieldMap;
+    }
+
+    public boolean isOption(String option) {
+        try {
+            optionExists(option, fieldMap);
+        } catch (PHPArrayReaderExeption e) {
+            return false;
+        }
+        ;
+        return true;
+    }
+
+    public boolean isOption(String[] options) {
+        try {
+            return isOption(options[options.length - 1], getArray(options));
+        } catch (PHPArrayReaderExeption e) {
+            return false;
+        }
+    }
+
+    private boolean isOption(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        return optionExists(option, fieldMap);
+    }
+
+    public Boolean isOptionString(String option) throws PHPArrayReaderExeption {
+        return isOptionString(option, fieldMap);
+    }
+
+    public Boolean isOptionString(String[] options) throws PHPArrayReaderExeption {
+        return isOptionString(options[options.length - 1], getArray(options));
+    }
+
+    private Boolean isOptionString(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        optionExists(option, fieldMap);
+        return getOption(option).getClass() == String.class;
+    }
+
+    public Boolean isOptionBoolean(String option) throws PHPArrayReaderExeption {
+        return isOptionBoolean(option, fieldMap);
+    }
+
+    public Boolean isOptionBoolean(String[] options) throws PHPArrayReaderExeption {
+        return isOptionBoolean(options[options.length - 1], getArray(options));
+    }
+
+    private Boolean isOptionBoolean(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        optionExists(option, fieldMap);
+        return getOption(option, fieldMap).getClass() == Boolean.class;
+    }
+
+    public Boolean isOptionArray(String option) throws PHPArrayReaderExeption {
+        return isOptionArray(option, fieldMap);
+    }
+
+    public Boolean isOptionArray(String[] options) throws PHPArrayReaderExeption {
+        return isOptionArray(options[options.length - 1], getArray(options));
+    }
+
+    private Boolean isOptionArray(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        optionExists(option, fieldMap);
+        return getOption(option, fieldMap).getClass() == HashMap.class;
+    }
+
+    public Object getOption(String option) throws PHPArrayReaderExeption {
+        return getOption(option, fieldMap);
+    }
+
+    public Object getOption(String[] options) throws PHPArrayReaderExeption {
+        return getOption(options[options.length - 1], getArray(options));
+    }
+
+    private Object getOption(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        optionExists(option, fieldMap);
+        return fieldMap.get(option);
+    }
+
+    public String getOptionString(String option) throws PHPArrayReaderExeption {
+        return getOptionString(option, fieldMap);
+    }
+
+    public String getOptionString(String[] options) throws PHPArrayReaderExeption {
+        return getOptionString(options[options.length - 1], getArray(options));
+    }
+
+    private String getOptionString(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        if (isOptionString(option, fieldMap)) return (String) getOption(option, fieldMap);
+        throw new PHPArrayReaderExeption("Is not a String");
+    }
+
+    public Boolean getOptionBoolean(String option) throws PHPArrayReaderExeption {
+        return getOptionBoolean(option, fieldMap);
+    }
+
+    public Boolean getOptionBoolean(String[] options) throws PHPArrayReaderExeption {
+        return getOptionBoolean(options[options.length - 1], getArray(options));
+    }
+
+    private Boolean getOptionBoolean(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        if (isOptionBoolean(option, fieldMap)) return (Boolean) getOption(option, fieldMap);
+        throw new PHPArrayReaderExeption(("is not a boolean"));
+    }
+
+    public Map<String, Object> getOptionArray(String option) throws PHPArrayReaderExeption {
+        return getOptionArray(option, fieldMap);
+    }
+
+    public Map<String, Object> getOptionArray(String[] options) throws PHPArrayReaderExeption {
+        return getOptionArray(options[options.length - 1], getArray(options));
+    }
+
+    private Map<String, Object> getOptionArray(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        if (isOptionArray(option, fieldMap)) return (Map<String, Object>) getOption(option, fieldMap);
+        throw new PHPArrayReaderExeption("Is not an array");
+    }
+
+    private boolean optionExists(String option) throws PHPArrayReaderExeption {
+        return optionExists(option, fieldMap);
+    }
+
+    private boolean optionExists(String[] options) throws PHPArrayReaderExeption {
+        return optionExists(options[options.length - 1], getArray(options));
+    }
+
+    private boolean optionExists(String option, Map<String, Object> fieldMap) throws PHPArrayReaderExeption {
+        if (fieldMap.containsKey(option) == false) {
+            throw new PHPArrayReaderExeption("Option does not exist");
+        }
+        return true;
+    }
+
+    private void fillArrayFieldStructure() {
+        pointer = 0;
+        int endPointer = phpArraySerial.length();
+        pointer++;//skip a
+        pointer++; //skip :
+        pointer++;//skip number
+        if (phpArraySerial.charAt(pointer) >= '0' && phpArraySerial.charAt(pointer) <= '9') pointer++;
+        if (phpArraySerial.charAt(pointer) >= '0' && phpArraySerial.charAt(pointer) <= '9') pointer++;
+        pointer++; //skip :
+        pointer++; //skip {
+        while (phpArraySerial.charAt(pointer) != '}') {
+            pointer++;//skip s fieldname identifier
+            String fieldName = getStringData();
+            //start value field
+            char type = phpArraySerial.charAt(pointer);
+            pointer++; //skip value identifier
+            if (type == 's') {
+                fieldMap.put(fieldName, getStringData());
+            } else if (type == 'b') {
+                fieldMap.put(fieldName, getBooleanData());
+            } else if (type == 'a') {
+                fieldMap.put(fieldName, getArrayData());
+            }
+        }
+        return;
+    }
+
+    private Map<String, Object> getArrayData() {
+        Map<String, Object> fieldNames = new HashMap<>();
+        String fieldName;
+        pointer++; //skip :
+        int arrayLength = Character.getNumericValue(phpArraySerial.charAt(pointer));
+        pointer++;
+        if (phpArraySerial.charAt(pointer) >= '0' && phpArraySerial.charAt(pointer) <= '9') {
+            arrayLength = arrayLength * 10 + Character.getNumericValue(phpArraySerial.charAt(pointer));
+            pointer++;
+        }
+        if (phpArraySerial.charAt(pointer) >= '0' && phpArraySerial.charAt(pointer) <= '9') {
+            arrayLength = arrayLength * 10 + Character.getNumericValue(phpArraySerial.charAt(pointer));
+            pointer++;
+        }
+        pointer++; //skip:
+        pointer++; //skip {
+        int numberFields = 0;
+        while (numberFields < arrayLength) {
+            pointer++; //skip s fieldname identifier
+            fieldName = getStringData();
+            char type = phpArraySerial.charAt(pointer);
+            pointer++; //skip value identifier
+            if (type == 's') {
+                fieldNames.put(fieldName, getStringData());
+            } else if (type == 'b') {
+                fieldNames.put(fieldName, getBooleanData());
+            } else if (type == 'a') {
+                fieldNames.put(fieldName, getArrayData());
+            }
+            numberFields++;
+        }
+        pointer++;//skip }
+        return fieldNames;
+    }
+
+    private String getStringData() {
+        pointer++; // skip :
+        int stringLength = Character.getNumericValue(phpArraySerial.charAt(pointer));
+        pointer++;
+        if (phpArraySerial.charAt(pointer) >= '0' && phpArraySerial.charAt(pointer) <= '9') {
+            stringLength = stringLength * 10 + Character.getNumericValue(phpArraySerial.charAt(pointer));
+            pointer++;
+        }
+        if (phpArraySerial.charAt(pointer) >= '0' && phpArraySerial.charAt(pointer) <= '9') {
+            stringLength = stringLength * 10 + Character.getNumericValue(phpArraySerial.charAt(pointer));
+            pointer++;
+        }
+        pointer++; //skip:
+        pointer++; //skip "
+        String stringData = phpArraySerial.substring(pointer, pointer + stringLength);
+        pointer += stringLength;
+        pointer++; //skip "
+        pointer++; //skip ;
+        return stringData;
+    }
+
+    private Boolean getBooleanData() {
+        pointer++;//skip :
+        Boolean booleanData;
+        if (phpArraySerial.charAt(pointer) == '0') {
+            booleanData = false;
+        } else {
+            booleanData = true;
+        }
+        pointer++;
+        pointer++; //skip ;
+        return booleanData;
+    }
+
+}
+
+
+
+
---
+++ src/nl/famschneider/PHPSerializedDataReader/PHPArrayReaderExeption.java
@@ -0,0 +1,11 @@
+package nl.famschneider.PHPSerializedDataReader;
+
+public class PHPArrayReaderExeption extends Exception {
+
+    public PHPArrayReaderExeption() {
+    }
+
+    public PHPArrayReaderExeption(String message) {
+        super(message);
+    }
+}
